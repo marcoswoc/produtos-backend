daffe7367ee0d26b6d6d89a6bd6f02012d898aad
marcos <marcoswoc@outlook.com>
2025-03-04 19:04:11 -0300

    Adicionando configurations product

---
+++ Produtos.Persistence/Configurations/Base/BaseMap.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Produtos.Domain.Entities.Base;
+
+namespace Produtos.Persistence.Configurations.Base;
+public abstract class BaseMap<TEntity> : IEntityTypeConfiguration<TEntity>
+    where TEntity : Entity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(entity => entity.Id);
+
+        builder.Property(entity => entity.CreatedAt)
+            .HasColumnType("DATETIME")
+            .IsRequired();
+
+        builder.Property(entity => entity.UpdatedAt)
+            .HasColumnType("DATETIME")
+            .HasDefaultValue(null);
+
+        builder.Property(entity => entity.DeletedAt)
+            .HasColumnType("DATETIME")
+            .HasDefaultValue(null);
+        
+        builder.Property(entity => entity.IsDeleted)            
+            .HasDefaultValue(false)            
+            .IsRequired();        
+    }
+}
+
---
+++ Produtos.Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Produtos.Domain.Entities;
+using Produtos.Persistence.Configurations.Base;
+
+namespace Produtos.Persistence.Configurations;
+
+public class ProductConfiguration : BaseMap<Product>
+{
+    public override void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(product => product.Id);
+
+        builder.Property(product => product.Name)
+            .HasMaxLength(200)
+            .IsRequired();
+
+        builder.Property(product => product.Description)
+            .HasMaxLength(500)
+            .IsRequired(false);
+
+        builder.Property(product => product.Price)
+            .HasPrecision(18, 2)
+            .IsRequired();
+
+        builder.Property(product => product.Category)
+            .HasConversion<string>()
+            .IsRequired();
+
+        builder.Property(product => product.ImageUrl)
+            .HasMaxLength(500);
+
+        base.Configure(builder);
+    }
+}
+
